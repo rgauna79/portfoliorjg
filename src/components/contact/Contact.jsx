ceac9a7de366fdcd4ee9a3b33c32a00afbb244f1
rgauna79 <rgauna@gmail.com>
2023-10-17 12:31:42 -0600

    creating contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,35 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contact-main">
+      <div className='contact-container'>
+        <div className="form-container">
+          <h2 className='contact-title'>Contact Me</h2>
+          <form action="" className="contact-form"></form>
+            <fieldset>
+              <label htmlFor="">Name: </label>
+              <input type="text" name="" id="" />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="">Email: </label>
+              <input type="email" name="" id="" />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="">Message</label>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </fieldset>
+            <input type="submit" value="" />
+        </div>
+        <div className="socialMedia">
+          <h3>My Social Media</h3>
+          <ul>
+            <li><a href=''>Github</a></li>
+            <li><a href=''>Gmail</a></li>
+            <li><a href=''>Linkedin</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
