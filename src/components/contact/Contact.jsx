10bc0f017cf5dc5ad416df2b6422a8af64719ceb
rgauna79 <rgauna@gmail.com>
2023-10-23 12:28:58 -0600

    Add emailjs to Contact Form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,26 +1,63 @@
-import React from 'react'
-import './contact.css'
+import React, {useRef} from 'react';
+import './contact.css';
+import emailjs from '@emailjs/browser';
+
+//Build a modal window to show the Succesful message
+const showModalOk = () => {
+    alert('message sent succesfully');
+
+
+}
+
 
 const Contact = () => {
+
+  const refForm = useRef();
+
+  // Handle form information 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const serviceId = "service_064reeh";
+    const templateId = "template_ao7mfvp";
+    
+    const apiKey = "YDA6pojR1x78PmXQ0";
+
+    emailjs.sendForm(serviceId,templateId,refForm.current,apiKey)
+      .then(result => { 
+        if (result.text === 'OK')
+        {
+          showModalOk();
+          refForm.current.reset();
+        }
+        
+      })
+      .catch(error => console.error(error))
+  }
+
   return (
     <section className="contact-main">
         <div className="form-container">
           <h2 className='contact-title'>Contact Me</h2>
-          <form action="" className="contact-form"></form>
+          <form ref={refForm} onSubmit={handleSubmit} className="contact-form">
             <fieldset>
-              <label htmlFor="name">Name: </label>
-              <input type="text" name="name" id="name" />
+              <label htmlFor="name" className='symbol-required'>Name: </label>
+              <input type="text" name="username" id="name" placeholder='Enter your name' required/>
             </fieldset>
             <fieldset>
-              <label htmlFor="email">Email: </label>
-              <input type="email" name="email" id="email" />
+              <label htmlFor="email" className='symbol-required'>Email: </label>
+              <input type="email" name="email" id="email" placeholder='Ex. yourname@dom.com' required/>
             </fieldset>
             <fieldset>
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" cols="30" rows="10"></textarea>
+              <label htmlFor="message" className='symbol-required' >Message</label>
+              <textarea maxLength="500" name="message" id="message" cols="30" rows="10" placeholder='Insert your message'></textarea>
             </fieldset>
             <button type="submit" value="" id="submit-contact" >Submit</button>
+            </form>
         </div>
+
+
+
 
       <div className="social-media">
           <h3>My Social Media</h3>
@@ -29,7 +66,7 @@ const Contact = () => {
             <li className='social-media-a-links'><a href='mailto:rgauna@gmail.com' target='_blank'>Gmail</a></li>
             <li className='social-media-a-links'><a href='https://linkedin.com/in/rgauna' target='_blank'>Linkedin</a></li>
           </ul>
-        </div>
+      </div>
     </section>
   )
 }
