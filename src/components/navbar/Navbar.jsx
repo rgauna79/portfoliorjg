69bf68c15383f3241c0ae86f7c9bbd8d3f8fabfd
rgauna79 <rgauna@gmail.com>
2023-10-24 12:00:40 -0600

    Making Navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  }
+
+
+
   return (
     <div className="navbar">
     <p>Roberto Gauna</p>
-    <ul className="navbar-links">
+    <div className="bar-menu">
+    <div className={`navbar-hamburger ${showMobileMenu ? 'active' : ''}`} onClick={toggleMobileMenu}>
+      <FontAwesomeIcon icon={faBars} />
+    </div>
+    <ul className={`navbar-links ${showMobileMenu ? 'show' : ''}`}>
       <li className="navbar-a-links">
         <a href="#home">Home</a>
       </li>
@@ -22,8 +37,14 @@ const Navbar = () => {
         <a href="">Contact</a>
       </li>
     </ul>
+
+    </div>
+
     </div>
   )
 }
+
+
+
 
 export default Navbar
