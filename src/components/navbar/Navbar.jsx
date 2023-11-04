f932c72a07eb17e4d7740e31263450d1053f4f45
rgauna79 <rgauna@gmail.com>
2023-11-03 23:00:54 -0600

    Adjusting width NavBar and Footer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,32 +14,35 @@ const Navbar = () => {
 
 
   return (
+    
     <div className="navbar">
-    <p>Roberto Gauna</p>
-    <div className="bar-menu">
-    <div className={`navbar-hamburger ${showMobileMenu ? 'active' : ''}`} onClick={toggleMobileMenu}>
-      <FontAwesomeIcon icon={faBars} />
-    </div>
-    <ul className={`navbar-links ${showMobileMenu ? 'show' : ''}`}>
-      <li className="navbar-a-links">
-        <a href="#home" onClick={toggleMobileMenu}>Home</a>
-      </li>
-      <li className="navbar-a-links">
-        <a href="#about" onClick={toggleMobileMenu}>About</a>
-      </li>
-      <li className="navbar-a-links">
-        <a href="#projects" onClick={toggleMobileMenu}>Portfolio</a>
-      </li>
-      <li className="navbar-a-links">
-        <a href="#skills" onClick={toggleMobileMenu}>Skills</a>
-      </li>
-      <li className="navbar-a-links">
-        <a href="#contact" onClick={toggleMobileMenu}>Contact</a>
-      </li>
-    </ul>
+        <div className="navbar-container">  
+          <p>Roberto Gauna</p>
+          <div className="bar-menu">
+          <div className={`navbar-hamburger ${showMobileMenu ? 'active' : ''}`} onClick={toggleMobileMenu}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+          <ul className={`navbar-links ${showMobileMenu ? 'show' : ''}`}>
+            <li className="navbar-a-links">
+              <a href="#home" onClick={toggleMobileMenu}>Home</a>
+            </li>
+            <li className="navbar-a-links">
+              <a href="#about" onClick={toggleMobileMenu}>About</a>
+            </li>
+            <li className="navbar-a-links">
+              <a href="#projects" onClick={toggleMobileMenu}>Portfolio</a>
+            </li>
+            <li className="navbar-a-links">
+              <a href="#skills" onClick={toggleMobileMenu}>Skills</a>
+            </li>
+            <li className="navbar-a-links">
+              <a href="#contact" onClick={toggleMobileMenu}>Contact</a>
+            </li>
+          </ul>
 
-    </div>
+          </div>
 
+        </div>
     </div>
   )
 }
